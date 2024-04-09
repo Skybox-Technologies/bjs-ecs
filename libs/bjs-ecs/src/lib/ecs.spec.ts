f31f6e143b2c83c0610cc923fe1d78a4613e9a6c
Pelle Johnsen <pelle.johnsen@skybox.gg>
2024-04-09 13:47:25 +0200

    tets: add more ecs unit tests

--- libs/bjs-ecs/src/lib/ecs.spec.ts
+++ libs/bjs-ecs/src/lib/ecs.spec.ts
@@ -1,9 +1,55 @@
-import { addEntity } from './ecs';
+import { addEntity, queryEntities, removeEntity } from './ecs';
 
-describe('bjsEsc', () => {
-  it('addEntity', () => {
-    const ent = addEntity(['test']);
-    expect(ent).toBeDefined();
-    expect(ent.is('test')).toBe(true);
+function door(isLocked: boolean) {
+  return { id: 'door', locked: isLocked };
+}
+door.id = 'door';
+
+function color(hex: string) {
+  return { id: 'color', color: hex };
+}
+color.id = 'color';
+
+describe('handle entities and queries', () => {
+  it('can add entities with tags', () => {
+    const player = addEntity(['actor', 'player']);
+    expect(player).toBeDefined();
+    expect(player.is('player')).toBe(true);
+
+    // add 10 enemies
+    for (let i = 0; i < 10; i++) {
+      const enemy = addEntity(['actor', 'enemy']);
+      expect(enemy).toBeDefined();
+      expect(enemy.is('enemy')).toBe(true);
+      expect(enemy.is('player')).toBe(false);
+    }
+  });
+
+  it('can add entities with components', () => {
+    const redDoor = addEntity([door(true), color('#ff0000')]);
+    expect(redDoor).toBeDefined();
+    expect(redDoor.locked).toBe(true);
+    expect(redDoor.color).toBe('#ff0000');
+  });
+
+  it('can query entities by tags', () => {
+    const entities = queryEntities(['actor']);
+    expect(entities.length).toBe(11);
+  });
+
+  it('can query entities by component', () => {
+    const entities = queryEntities([color]);
+    expect(entities.length).toBe(1);
+    expect(entities[0].color).toBe('#ff0000');
+  });
+
+  it('can remove entities', () => {
+    let entities = queryEntities(['enemy']);
+    expect(entities.length).toBe(10);
+
+    removeEntity(entities[0]);
+
+    entities = queryEntities(['enemy']);
+    expect(entities.length).toBe(9);
   });
 });
